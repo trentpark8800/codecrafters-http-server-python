5e1fdc7b9ac8901d306883de643f7e72e65c2382
trentpark8800@gmail.com <trentp@datageek.co.za>
2026-07-02 07:57:34 +0200

    Add 200 response

--- app/main.py
+++ app/main.py
@@ -5,10 +5,11 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # TODO: Uncomment the code below to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4221))
+    conn, addr = server_socket.accept() # wait for client
+    print("Got connection from", addr )
+
+    conn.send(r"HTTP/1.1 200 OK\r\n\r\n".encode())
 
 
 if __name__ == "__main__":
