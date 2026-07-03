4744d8d4789e93fbbd84133c121bfb725c1b886d
trentpark8800@gmail.com <trentpark8800@gmail.com>
2026-07-03 07:51:21 +0200

    Add basic request-response pattern

--- app/main.py
+++ app/main.py
@@ -1,4 +1,37 @@
-import socket  # noqa: F401
+import socket
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+
+@dataclass
+class Request:
+    http_method: str
+    target: str
+    http_version: str
+    # headers: Dict[str, Any]
+
+
+def request_service(data: bytes) -> Request:
+    
+    split_data: List[bytes] = data.split(b" ")
+
+    request: Request = Request(
+        http_method=split_data[0],
+        target=split_data[1],
+        http_version=split_data[2],
+    )
+
+    return request
+
+
+def response_service(request: Request) -> bytes:
+    
+    if request.target == "/":
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    
+    return response
 
 
 def main():
@@ -9,7 +42,13 @@ def main():
     conn, addr = server_socket.accept() # wait for client
     print("Got connection from", addr )
 
-    conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            request: Request = request_service(data=data)
+            response: bytes = response_service(request=request)
+
+            conn.send(response)
 
 
 if __name__ == "__main__":
